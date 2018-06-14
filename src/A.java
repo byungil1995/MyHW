f381994174328a944bbd7137eb6d305028f52b55
byungil1995 <byungil1995@gmail.com>
2018-06-14 15:32:47 +0900

    devC version

--- src/A.java
+++ src/A.java
@@ -1,5 +1,5 @@
 public class A {                                                                    
     public static void main(String[] args) {
-        System.out.println("Hello World computer software class 15");
+        System.out.println("Hello World computer software class 15 this is a public test scenario");
     }
 }
--- src/C.java
+++ src/C.java
@@ -1,5 +1,5 @@
 public class C {                                                                    
     public static void main(String[] args) {
-        System.out.println("Hello World 3");
+        System.out.println("Hello World this is a homework test version");
     }
 }
